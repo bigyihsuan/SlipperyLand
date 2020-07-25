6a1df0d9934ec532e388aaea43e629d6bdc3868e
bigyihsuan <bigyihsuan@gmail.com>
2020-07-24 23:54:18 -0400

    initial commit

---
+++ Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+
+using HarmonyLib;
+using UnityModManagerNet;
+using UnityEngine;
+
+namespace SlipperyLand
+{
+	public class Main
+	{
+		static void Load(UnityModManager.ModEntry modEntry)
+		{
+			var harmony = new Harmony(modEntry.Info.Id);
+			harmony.PatchAll();
+		}
+	}
+
+	[HarmonyPatch(typeof(TrainCarColliders), "BogiesPhysicMaterial")]
+	[HarmonyPatch(MethodType.Getter)]
+	class TrainCarColliders_BogiesPhysicMaterial_Getter_Patch
+	{
+		static void Postfix(ref PhysicMaterial __result)
+		{
+			if ((UnityEngine.Object) __result != (UnityEngine.Object) null)
+			{
+				__result.dynamicFriction = Single.Epsilon;
+			}
+		}
+	}
+	[HarmonyPatch(typeof(TrainCarColliders), "TrainCarPhysicMaterial")]
+	[HarmonyPatch(MethodType.Getter)]
+	class TrainCarColliders_TrainCarPhysicMaterial_Getter_Patch
+	{
+		static void Postfix(ref PhysicMaterial __result)
+		{
+			if ((UnityEngine.Object) __result != (UnityEngine.Object) null)
+			{
+				__result.dynamicFriction = Single.Epsilon;
+			}
+		}
+	}
+}
